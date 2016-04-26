5453c416cd0d0813102a27b274134708a386fed7
Benics Balázs <benicsbalazs@hotmail.com>
2016-04-26 23:38:07 +0200

    Update main.hs

--- CNF/main.hs
+++ CNF/main.hs
@@ -2,9 +2,20 @@ import Types
 import GrammarParser
 import System.Environment
 
---Only one arg: filePath for reading
 main :: IO ()
 main = do
-       [inputPath] <- getArgs
+       [inputPath, outputPath] <- getArgs
        fileContent <- readFile inputPath
-       print $parseGrammar fileContent
+       writeFile outputPath $grammarToString $stringToGrammar fileContent
+       print $stringToGrammar fileContent
+
+
+----Only one arg: filePath for reading
+--main :: IO ()
+--main = do 
+--       run getArgs
+--
+--run :: [String] -> IO()
+--run [inputPath] = print $stringToGrammar $readFile inputPath
+--run [inputPath, outputPath] = writeFile outputPath $grammarToString $stringToGrammar $readFile inputPath
+--run _ = error "A program futtatasahoz legalabb egy argumentum szukseges. Annak pedig a beolvasando fajl eleresi cimenek kell lennie.";
