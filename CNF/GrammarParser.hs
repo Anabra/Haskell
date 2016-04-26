f94a200c179d98697d41c6a35e18e71e72bfba92
Benics Balázs <benicsbalazs@hotmail.com>
2016-04-26 23:39:10 +0200

    Update GrammarParser.hs

--- CNF/GrammarParser.hs
+++ CNF/GrammarParser.hs
@@ -1,44 +1,71 @@
-module GrammarParser (parseGrammar) where
+module GrammarParser (stringToGrammar, grammarToString) where
 
 import Types
+import Data.List
 import Data.List.Split
 
-parseGrammar :: String -> Grammar
-parseGrammar fileContent = (s, e, ns, ts, rs) --fileContent == "S;epszilon;A,B,C,D,S;a,b,epszilon\nS->a,A|a,D|b,C|epszilon\nA->a,B|b,A|b,C"
+
+-------------------------------Decode Grammar from String-------------------------------
+
+stringToGrammar :: String -> Grammar
+stringToGrammar fileContent = (s, e, ns, ts, rs)
   where
-    lines = splitOn "\n" fileContent -- lines == [ "S;epszilon;A,B,C,D,S;a,b,epszilon", "S->a,A|a,D|b,C|epszilon", "A->a,B|b,A|b,C" ]
-    [s,e,n,t] = splitOn ";" $ head lines --s == "S", e == "epszilon", n == "A,B,C,D,S", t == "a,b,epszilon"
-    ns = splitOn "," n --ns == [ "A", "B", "C", "D", "S" ]
-    ts = splitOn "," t --ts == [ "a", "b", "epszilon" ]
-    ruleLineStrings = tail lines --ruleLineStrings == [ "S->a,A|a,D|b,C|epszilon", "A->a,B|b,A|b,C" ,... ]
-    rs = map parseRuleString $tail lines --rs == [("S", [ ["a", "A"], ["a", "D"], ["b", "C"], ["epszilon"] ]), ("A", [ ["a", "B"], ["b", "A"], ["b", "C"] ])]
+    lines = splitOn "\n" fileContent
+    [s,e,n,t] = splitOn ";" $ head lines
+    ns = splitOn "," n
+    ts = splitOn "," t
+    ruleLineStrings = tail lines
+    rs = map stringToRule $tail lines
 
-parseRuleString :: String -> Rule--"S->a,A|a,D|b,C|epszilon" -> ("S", [ ["a", "A"], ["a", "D"], ["b", "C"], ["epszilon"] ] )
-parseRuleString line = (s, words)
+stringToRule :: String -> Rule
+stringToRule line = (s, words)
   where
-    [s, wordString] = splitOn "->" line      --s == "S", wordString == "a,A|a,D|b,C|epszilon"
-    wordStrings = splitOn "|" wordString     --WordString == ["a,A", "a,D", "b,C", "epszilon"]
-    words = map parseWordString wordStrings  --words == [ ["a", "A"], ["a", "D"], ["b", "C"], ["epszilon"] ] :: [WorD]
+    [s, wordString] = splitOn "->" line
+    wordStrings = splitOn "|" wordString
+    words = map stringToWord wordStrings
 
-parseWordString :: String -> WorD                    --Eg: "a,A" -> ["a", "A"]
-parseWordString wordString = splitOn "," wordString
+stringToWord :: String -> WorD
+stringToWord wordString = splitOn "," wordString
 
 
+-------------------------------Encode Grammar to String-------------------------------
 
---STARTSYMBOL;EMPTYSYMBOL;NONTERMINALS;TERMINALS\n   <-- separated by commas
---NONTERMINAL->SYMBOLS|SYMBOLS|SYMBOLS\n
---NONTERMINAL->SYMBOLS|SYMBOLS|SYMBOLS\n
---etc...
+grammarToString :: Grammar -> String
+grammarToString (s, e, ns, ts, rs) = intercalate ";" [ s, e, ns', ts' ] ++ "\n" ++ rs'
+  where
+    ns' = wordToString ns
+    ts' = wordToString ts
+    rs' = init $unlines $map ruleToString rs
+
+ruleToString :: Rule -> String
+ruleToString (s, words) = s ++ "->" ++ intercalate "|" ( map wordToString words )
+
+wordToString :: [Symbol] -> String
+wordToString ss = intercalate "," ss
+
+
+--Important!
+
+--Format of the input.
+--The format of the content is realy strict, so don't miss anything.
+--The fist line contains the start symbol, empty word symbol nonterminal symbols and the terminal symbols.
+--As you can see, after the start and empty symbols there is a semicolon. It is also true for nonterminals and terminals,
+--but the nonterminals and terminals are separated by a simple colon.
+--At the end of the line there is no sperarator. (It is also true for all lines, even the last line, becouse that line does not end with new line tag. (\n))
+--The rules of the grammar are in the next lines (after the first line), in the following format:
+--Every line starts with the start symbol of the rule followed by "->" sign and the list of words.
+--Each word separated by "|" sign, and if the word contains multiple symbols, than those are separated by colon from each other.
+--As you can see, these lines end only with new line symbols (\n), except the last line.
+
+
+--Common mistakes:
+--First line semicolon, colon mismatch,...
+--Line endings.
+--No new line signs at the end of the rule line (except last line)
+
 
 {- Possible input:
 S;epszilon;A,B,C,D,S;a,b,epszilon
 S->a,A|a,D|b,C|epszilon
 A->a,B|b,A|b,C
--}
-
-{-Interpreted as: Grammar
-("S","epszilon",["A","B","C","D","S"],["a","b","epszilon"],
-
-    [("S",[["a","A"],["a","D"],["b","C"],["epszilon"]]),
-    ("A",[["a","B"],["b","A"],["b","C"]])])
 -}
