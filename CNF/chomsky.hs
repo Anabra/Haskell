8e20048e4b7fb03b1c1c8d079b76394eb48f4615
Benics Balázs <benicsbalazs@hotmail.com>
2016-04-26 23:53:42 +0200

    Podlovics not reviewed

--- CNF/chomsky.hs
+++ CNF/chomsky.hs
@@ -3,6 +3,7 @@ import Data.List
 type Symbol = [Char] --a whole string can represent a single symbol
 type WorD = [Symbol]
 type Rule = (Symbol,[WorD]) --only 2nd type grammar is appropriate /// must give all right hand sides for a rule
+type Rule' = (Symbol, WorD) --we will have to break down each rule into rules like this (S -> [["a"],["b"]]   ==>   [S -> ["a"]  ,  S -> ["b"]])
 type Grammar = (Symbol, Symbol, [Symbol], [Symbol], [Rule]) --(S, epsilon, N, T, R)
 
 
@@ -201,10 +202,73 @@ isChomsky grammar = noEps && rulesOK
     (start, eps, nonterminals, terminals, rules) = grammar
     
     
+ -------------------------------------------------------------------------------CYK  ALGORYTHM-------------------------------------------------------------------------------------
+
+breakdown :: Rule -> [Rule']
+breakdown rule@(lhs,rhs) = [ (lhs, subrule) | subrule <- rhs ]
+
+breakdownRules :: Grammar -> [Rule']
+breakdownRules grammar = concatMap (breakdown) rules
+    where
+    (_, _, _, _, rules) = grammar
+
+
+--the the input lists should be sorted
+merge :: (a -> a -> Bool) -> [a] -> [a] -> [a]
+merge _ [] list2 = list2
+merge _ list1 [] = list1
+merge rel list1@(x:xs) list2@(y:ys)
+    | rel x y   = x : merge rel xs list2
+    | otherwise = y : merge rel list1 ys
     
+mergesort :: (a -> a -> Bool) -> [a] -> [a]
+mergesort _ [x] = [x]
+mergesort rel xs = merge rel (mergesort rel firstHalf) (mergesort rel secondHalf)
+    where
+    half = length xs `div` 2
+    firstHalf = take half xs
+    secondHalf = drop half xs
+
+
+descartes :: [[a]] -> [[a]]
+descartes [] = [[]]
+descartes (x:xs) = [  a : dx | a <- x, dx <- descartes xs]
+
+
+sliceWord :: [a] -> [([a], [a])]
+sliceWord word@(x:xs) = slcwrdHelper [x] xs
+    where
+    slcwrdHelper :: [a] -> [a] -> [([a], [a])]
+    slcwrdHelper xs [y] = [(xs, [y])]
+    slcwrdHelper xs other@(y:ys) = (xs, other) : slcwrdHelper (xs ++ [y]) ys
+    --slcwrdHelper [x] ys = [([x], ys)]
+    --slcwrdHelper xs ys = (xs, ys) : slcwrdHelper (init xs) (last xs : ys)
     
+--filter rules a b == c // a -> rhs to search for; b -> rules to filter (sorted by rhs); c -> lhs-s of each rule that has passed the query
+searchForWord :: WorD -> [Rule'] -> [Symbol]
+searchForWord word rules@(r:rs)
+    | word < rhs = []
+    | word > rhs = searchForWord word rs
+    | otherwise = lhs : searchForWord word rs
+    where (lhs, rhs) = r
+
+sortedbrules = mergesort (\(_,a) (_,b) -> a < b) $ breakdownRules $ chomsky grammar1
     
+
+cyk :: [Rule'] -> WorD -> [Symbol]
+cyk rules [t] = searchForWord [t] rules
+cyk rules word = concatMap (flip (searchForWord) rules) descartesWords
+    where
+    descartesWords = concat [ descartes [cyk rules pre, cyk rules suf] | w@(pre,suf) <- sliceWord word]
     
+    --searchForWord ["a"] ( mergesort (\(_,a) (_,b) -> a < b) (breakdownRules (chomsky grammar1)))
+    
+isInLanguage :: WorD -> Grammar -> Bool
+isInLanguage word grammar = start `elem` (cyk rules' word)
+    where
+    rules' = mergesort (\(_,a) (_,b) -> a < b) $ breakdownRules $ grammar
+    (start, eps, nonterminals, terminals, rules) = chomsky grammar
+
     
  -------------------------------------------------------------------------------EXAMPLE INPUTS-------------------------------------------------------------------------------------
    
