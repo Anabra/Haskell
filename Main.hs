e4936c71a347b1bad99bf9ceed238ecfdebe8f00
Anabra <peter.d.podlovics@gmail.com>
2016-04-28 12:43:29 +0200

    Main recompiled
    It no longer needs an output file. A runtime spot (-rtspot) has been complied into the EXE. Use the +RTS -s flag for statistics.

--- Main.hs
+++ Main.hs
@@ -6,14 +6,13 @@ import Cyk
 
 main :: IO ()
 main = do
-       [inputPath, inputWordsPath, outputPath] <- getArgs
+       [inputPath, inputWordsPath] <- getArgs
        fileContent <- readFile inputPath
        inputWords <- readFile inputWordsPath
        let words = [ stringToWord w | w <- lines inputWords]
        let parsedGrammar = chomsky $ stringToGrammar fileContent
        --let subresult = [ cyk sortedbrules w | w <- words]
        let result = [ isInLanguage w parsedGrammar | w <- words]
-       --writeFile outputPath $grammarToString $stringToGrammar fileContent
        print words
        print parsedGrammar
        print result
